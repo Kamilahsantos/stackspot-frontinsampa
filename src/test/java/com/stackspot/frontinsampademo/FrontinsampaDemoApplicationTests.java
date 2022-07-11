0364e8938667f4ae56b5a06121b5993a7d45d9c2
Kamila Santos <kahsantos89@gmail.com>
2022-07-11 10:21:10 -0300

    projeto base

---
+++ src/test/java/com/stackspot/frontinsampademo/FrontinsampaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackspot.frontinsampademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontinsampaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
